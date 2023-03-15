faaf72badf4bc4dae12e9155831a9b353e633e5a
MCYhhh <3258659445@qq.com>
2023-03-15 21:16:11 +0800

    购物界面初版

---
+++ agriculture-front/src/views/devviews/Cart.vue
@@ -0,0 +1,16 @@
+<template>
+<!--  购物页面-->
+  <div class="cart">
+<!--    header头部区-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cart"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ agriculture-front/src/views/devviews/CartHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="cart-header">{{title}}</div>
+</template>
+
+<script>
+export default {
+  name: "CartHeader",
+  data(){
+    return{
+      title:'购物中心'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
