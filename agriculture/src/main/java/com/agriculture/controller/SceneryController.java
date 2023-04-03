0765c7edfd86c09b9d0e450266e60ece1cd3c811
yeshangmaitian <tianyong193@163.com>
2023-04-03 21:06:06 +0800

    增加user/save中用户密码的加密，以及景区景点的save，景区重复会提示错信息

--- agriculture/src/main/java/com/agriculture/controller/SceneryController.java
+++ agriculture/src/main/java/com/agriculture/controller/SceneryController.java
@@ -1,8 +1,16 @@
 package com.agriculture.controller;
 
 
+import com.agriculture.common.HttpCode;
+import com.agriculture.common.Result;
+import com.agriculture.entity.Scenery;
+import com.agriculture.service.ISceneryService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -15,5 +23,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/scenery")
 public class SceneryController {
+    @Resource
+    ISceneryService iSceneryService;
 
+    @PostMapping("/save")
+    public Result save(@RequestBody Scenery scenery){
+        for(Scenery a:iSceneryService.list()) {
+            if(scenery.getSname().equals(a.getSname())) {
+                return Result.error(HttpCode.USER_System.code(),"参数错误，景区已存在");
+            }
+        }
+        return Result.success(iSceneryService.save(scenery));
+    }
 }
