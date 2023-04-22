9979e9473615614389f0bf4e2f9de6b6572456a4
Syt24 <3298988338@qq.com>
2023-04-23 00:03:22 +0800

    files

---
+++ agriculture/src/main/java/com/agriculture/config/file/Files.java
@@ -0,0 +1,21 @@
+package com.agriculture.config.file;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+@Data
+//@TableName("sys_file")
+public class Files {
+
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+    private String name;
+    private String type;
+    private Long size;
+    private String url;
+    private String md5;
+    private Boolean isDelete;
+    private Boolean enable;
+
+}
