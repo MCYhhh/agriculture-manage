432bbb9b30b0b2afdcb23f70ab60cc42f40b5335
Syt24 <3298988338@qq.com>
2023-04-23 00:03:58 +0800

    FileMapper

---
+++ agriculture/src/main/java/com/agriculture/mapper/FileMapper.java
@@ -0,0 +1,10 @@
+package com.agriculture.mapper;
+
+import com.agriculture.config.file.Files;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+
+public interface FileMapper extends BaseMapper<Files> {
+}
