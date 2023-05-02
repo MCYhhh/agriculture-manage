f228ef8501e9b60332cfff003da5b74308ac1dcd
MCYhhh <3258659445@qq.com>
2023-05-03 00:07:57 +0800

    后端上传文件

--- agriculture/src/main/java/com/agriculture/config/cors/CorsConfig.java
+++ agriculture/src/main/java/com/agriculture/config/cors/CorsConfig.java
@@ -1,11 +1,16 @@
 package com.agriculture.config.cors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${file.path}")
+    private String filepath;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -21,5 +26,12 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 //跨域运行时间
                 .maxAge(3600);
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //将图片访问路径映射到D盘
+        registry.addResourceHandler("/file/**").addResourceLocations("file:"+filepath);
+//以下的写法表示将“/img/img/img/**”映射到项目里面resource.static.img,这只是个示例，相信你能够理解
+//        registry.addResourceHandler("/img/img/img/**").addResourceLocations("classpath:/static/img/**");
     }
 }
